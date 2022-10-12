b22d9c2b19b84614e39b08989c0b4aef964b134f
PTAHume <patricktahume@gmail.com>
2022-10-12 23:16:03 +0100

    adding mobile support

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'move.a.block',
+  appName: 'move-a-block',
+  webDir: 'dist/move-a-block',
+  bundledWebRuntime: false,
+};
+
+export default config;
